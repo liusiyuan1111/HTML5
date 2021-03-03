dc749c11560c858d24e00cfedd0e95fa3c28fc47
Kikuchan <792142895@qq.com>
2021-03-03 22:54:19 +0800

    。

--- 学生项目/伍赞威/项目/项目2/书本配套代码/《HTML5与CSS3权威指南》范例及代码/第10章/代码清单10-6/脚本文件/代码清单10-10/worker1.js
+++
@@ -1,8 +0,0 @@
-﻿onmessage = function(event) {
-var intArray=new Array(100);    
-for(var i=0;i<100;i++)
-    intArray[i]=parseInt(Math.random()*100);
-postMessage(JSON.stringify(intArray));
-close();
-}
-
